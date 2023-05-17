4e3101faf3855f1930016c57f5f4ae0cbb84ccea
Juarezgump655 <ajuarezcastellanos45@gmail.com>
2023-05-16 21:50:13 -0600

    Configuracion heroku

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = express();
 
 app.use(express.static('./dist/'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+app.get('*', (req, res) => {
+    res.sendFile(`./quejas-frontend/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
-
 app.listen(process.env.PORT || 8080);
