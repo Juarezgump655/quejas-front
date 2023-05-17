1d474ebfb26dbb51819186dd65159abef68e1171
Juarezgump655 <ajuarezcastellanos45@gmail.com>
2023-05-16 21:45:37 -0600

    configuracion Heroku

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/' });
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
